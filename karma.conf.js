5841bc50056118d0131eb0d7eab37a706268986d
Akash Gupta <agupta@i-sight.com>
2019-06-20 11:25:48 -0400

    karma update

--- karma.conf.js
+++ karma.conf.js
@@ -18,13 +18,13 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/test-angular-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
-	  fixWebpackSourcePaths: true,
-	  thresholds: {
-		statements: 80,
-		lines: 80,
-		branches: 80,
-		functions: 80
-	  }
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -32,7 +32,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      },
+    },
     singleRun: false,
     restartOnFileChange: true
   });
 };
+
